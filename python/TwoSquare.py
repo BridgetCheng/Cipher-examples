79e3031affe9336f6ce472df2aa4505dd346d67d
Bridget Cheng <bee.cheng@hotmail.com>
2015-07-04 18:59:02 +0800

    init cipyer table

---
+++ python/TwoSquare.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# Playfair cipher
+# 
+# @author  Orange 
+#
+
+#
+# 生成密表
+#
+def generateTable(key=''):
+	alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
+	table = [['A'] *5 for row in range(5)]
+	key1 = key1.upper()
+	key2 = key2.upper()
+	# for i in alphabet
+	# 	alphabet
+    
+
+
+#
+# 加密
+#
+def encrypt(key, words):
+    pass
+
+
+# 
+# 解密
+# 
+def decrypt(key, text):
+    pass
+
+
+# 获取字符在密表中的位置
+def position(table, ch):
+    pass
+
+if __name__ == '__main__':
+    # 明文
+    text = 'help me obi wan kenobi'
+    # 密匙
+    key1 = 'example'
+    key2 = 'keyword'
+
+
+
